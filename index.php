97a718b63cfd8c7e985f5c68c269bcc6285ec040
Mayank <m.kashyap@sapalomera.cat>
2024-02-03 12:56:38 +0100

    mayank

--- index.php
+++ index.php
@@ -1,13 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello Woeld</h1>
-    hufeufe
+    <title></title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOcGmgmJW5tsz1K5tqzVr7wFOPZkEQMAUD9PFZgbjN3Lzp8dU9Fp3hJT5iRwn" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
+    <!-- CSS -->
+    <link rel="stylesheet" href="Assets/Css/index.style.css">
+
+    <!-- js -->
+    <script src="Assets/Js/index.js"></script>
+
+    
+</head>
+
+<body>
+    <!-- NavBar -->
+    <header class="header-area overlay">
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">Wakeful Security</a>
+                <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#main-nav">
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                </button>
+
+                <div id="main-nav" class="collapse navbar-collapse justify-content-end">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                                </svg>
+                            </a>
+                            <div class="dropdown-menu custom-dropdown" aria-labelledby="navbarDropdown">
+                                <a href="#" class="dropdown-item">Inicio</a>
+                                <a href="#" class="dropdown-item">Nosotros</a>
+                                <a href="#" class="dropdown-item">Nuestros Servicios</a>
+                                <a href="#" class="dropdown-item">Categorias Professionales</a>
+                                <a href="#" class="dropdown-item">Wakeful Academy</a>
+                                <a href="#" class="dropdown-item">Clientes</a>
+                                <a href="#" class="dropdown-item">Blog</a>
+                                <a href="#" class="dropdown-item">Contacto</a>
+                                <a href="#" class="dropdown-item">Bolsa De Trabajo</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="banner">
+            <div class="container">
+                <h1>TU SEGURIDAD ES NUESTRA PRIORIDAD</h1>
+                <p>SEGURIDAD PRIVADA PARA EMPRESAS, PARTICULARES Y ENTIDADES</p>
+                <a href="#content" class="button button-primary">SABER MAS</a>
+            </div>
+        </div>
+    </header>
+
+    <!-- Content -->
+    <div class="container">
+        
+    </div>
+
 </body>
 </html>
